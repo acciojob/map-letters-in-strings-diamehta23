6b3862417518b6d04a5599693d945987d5763b13
diamehta23 <154620279+diamehta23@users.noreply.github.com>
2025-05-14 07:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+
+    if (!result[letter]) {
+      result[letter] = [];
+    }
+
+    result[letter].push(i);
+  }
+
+  return result;
+}
